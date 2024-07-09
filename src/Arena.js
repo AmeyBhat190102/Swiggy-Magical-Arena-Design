964ac9be976cc60b6ff3ac51b7a19d30c937ae23
AmeyBhat1901 <94900974+AmeyBhat190102@users.noreply.github.com>
2024-07-09 23:41:14 +0530

    Added Arena Class and Functions

---
+++ src/Arena.js
@@ -0,0 +1,45 @@
+const Player = require('./Player');
+
+class Arena {
+    constructor(playerA, playerB) {
+        this.playerA = playerA;
+        this.playerB = playerB;
+    }
+
+    rollDice() {
+        return Math.floor(Math.random() * 6) + 1;
+    }
+
+    startMatch() {
+        while (this.playerA.isAlive() && this.playerB.isAlive()) {
+            if (this.playerA.health < this.playerB.health) {
+                this.executeTurn(this.playerA, this.playerB);
+            } else {
+                this.executeTurn(this.playerB, this.playerA);
+            }
+        }
+        this.determineWinner();
+    }
+
+    executeTurn(attacker, defender) {
+        const attackRoll = this.rollDice();
+        const defendRoll = this.rollDice();
+
+        const damage = attacker.attackValue() * attackRoll - defender.defendValue() * defendRoll;
+        if (damage > 0) {
+            defender.takeDamage(damage);
+        }
+    }
+
+    determineWinner() {
+        if (this.playerA.isAlive()) {
+            console.log("Player A wins!");
+        } else if (this.playerB.isAlive()) {
+            console.log("Player B wins!");
+        } else {
+            console.log("It's a draw!");
+        }
+    }
+}
+
+export default Arena;
