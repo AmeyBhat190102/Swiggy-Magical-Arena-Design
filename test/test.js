d934cb35a9523dd7a2f8a48d3be4c1e8b81076af
AmeyBhat1901 <94900974+AmeyBhat190102@users.noreply.github.com>
2024-07-10 05:18:43 +0530

    Updated test.js with test comments

--- test/test.js
+++ test/test.js
@@ -2,12 +2,16 @@ const Player = require('../src/Player');
 const Arena = require('../src/Arena');
 
 function testPlayerCreation() {
+    console.log("Running testPlayerCreation test - ")
     const player = new Player(50, 5, 10);
     console.assert(player.health === 50, 'Player creation test failed');
     console.log('Player creation test passed');
+    console.log("*****************************************************");
+    console.log();
 }
 
 function testAttackAndDefense() {
+    console.log("Running testAttackAndDefense test - ")
     const playerA = new Player(50, 5, 10);
     const playerB = new Player(100, 10, 5);
     const arena = new Arena(playerA, playerB);
@@ -17,9 +21,12 @@ function testAttackAndDefense() {
     console.assert(playerA.isAlive(), 'Player A should be alive');
     console.assert(playerB.isAlive(), 'Player B should be alive');
     console.log('Attack and defense test passed');
+    console.log("*****************************************************");
+    console.log();
 }
 
 function testEqualAttributesMatch() {
+    console.log("Running testEqualAttributesMatch test - ")
     const playerA = new Player(50, 5, 10);
     const playerB = new Player(50, 5, 10);
     const arena = new Arena(playerA, playerB);
@@ -29,9 +36,12 @@ function testEqualAttributesMatch() {
     console.assert(playerA.isAlive() || playerB.isAlive(), 'At least one player should be alive');
     console.assert(!(playerA.isAlive() && playerB.isAlive()), 'Both players cannot be alive');
     console.log('Equal attributes match test passed');
+    console.log("*****************************************************");
+    console.log();
 }
 
 function testMaxDiceRolls() {
+    console.log("Running testMaxDiceRolls test = ")
     const playerA = new Player(100, 10, 10);
     const playerB = new Player(100, 10, 10);
     const arena = new Arena(playerA, playerB);
@@ -47,9 +57,12 @@ function testMaxDiceRolls() {
 
     console.assert(playerB.health === 40, 'Player B should have 40 health left');
     console.log('Max dice rolls test passed');
+    console.log("*****************************************************");
+    console.log();
 }
 
 function testMinDiceRolls() {
+    console.log("Runnning testMinDiceRolls test - ")
     const playerA = new Player(100, 10, 10);
     const playerB = new Player(100, 10, 10);
     const arena = new Arena(playerA, playerB);
@@ -65,9 +78,12 @@ function testMinDiceRolls() {
 
     console.assert(playerB.health === 100, 'Player B should have 100 health left');
     console.log('Min dice rolls test passed');
+    console.log("*****************************************************");
+    console.log();
 }
 
 function testZeroAttackOrDefense() {
+    console.log("Running testZeroAttackOrDefense test - ")
     const playerA = new Player(100, 10, 0);
     const playerB = new Player(100, 0, 10);
     const arena = new Arena(playerA, playerB);
@@ -78,9 +94,12 @@ function testZeroAttackOrDefense() {
     console.assert(playerA.health < 100, 'Player A should have less than 100 health');
     console.assert(playerB.health === 100, 'Player B should have 100 health');
     console.log('Zero attack or defense test passed');
+    console.log("*****************************************************");
+    console.log();
 }
 
 function testIdenticalAttributesProgression() {
+    console.log("Runnning testIdenticalAttributesProgression test - ")
     const playerA = new Player(50, 5, 10);
     const playerB = new Player(50, 5, 10);
     const arena = new Arena(playerA, playerB);
@@ -90,6 +109,8 @@ function testIdenticalAttributesProgression() {
     console.assert(playerA.isAlive() || playerB.isAlive(), 'At least one player should be alive');
     console.assert(!(playerA.isAlive() && playerB.isAlive()), 'Both players cannot be alive');
     console.log('Identical attributes progression test passed');
+    console.log("*****************************************************");
+    console.log();
 }
 
 testPlayerCreation();
